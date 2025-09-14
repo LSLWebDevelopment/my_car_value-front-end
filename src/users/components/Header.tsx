9364a985187990f162bd606fb872b396288e4375
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-14 10:00:51 -0300

    Changed the color or the header

--- src/users/components/Header.tsx
+++ src/users/components/Header.tsx
@@ -7,8 +7,8 @@ interface HeaderProps {
 export function Header({ children }: HeaderProps) {
   return (
     <header className="text-center mt-5">
-      {/* <h1 className="text-emerald-900 font-bold text-5xl">{children}</h1> */}
-      <h1 className="text-red-700 font-bold text-5xl">{children}</h1>
+      <h1 className="text-emerald-900 font-bold text-5xl">{children}</h1>
+      {/* <h1 className="text-red-700 font-bold text-5xl">{children}</h1> */}
     </header>
   );
 }
